ed5de861f6aa110e61f8b7a3ebdf2789736d9a1d
Anagh Kumar <54856646+anagh9@users.noreply.github.com>
2021-02-05 15:19:20 +0530

    Another Approach Added

--- Leet Code/TwoSum.cpp
+++ Leet Code/TwoSum.cpp
@@ -1,3 +1,5 @@
+// ProblemLink : https://leetcode.com/problems/two-sum/submissions/
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -10,9 +12,38 @@ public:
 
                 }
 
-                break;
             }
         }
         return result;
     }
 };
+
+// Time Complexity => O(n^2)
+// Space Complexity => O(1)
+
+// ----------------------------------------------------------------------
+
+/* Approach 2 Two Pass Hash Table */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> imap;
+    
+    for (int i = 0;; ++i) {
+        auto it = imap.find(target - nums[i]);
+        
+        if (it != imap.end()) 
+            return vector<int> {i, it->second};
+            
+        imap[nums[i]] = i;
+    }
+   
+    }
+        
+};
+
+
+
+// Time Complexity => O(n)
+// Space Complexity => O(n)
